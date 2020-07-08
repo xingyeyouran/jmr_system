105236d0dd87d6543ba6bce326e2f43affd509c3
鹏 刘 <152628liupeng@gmail.com>
2020-07-08 22:12:26 +0800

    redesign dateBase

--- jmr/forms.py
+++ jmr/forms.py
@@ -4,3 +4,5 @@ from django import forms
 class UserForm(forms.Form):
     user_name = forms.CharField(label='用户名', max_length=40)
     user_password = forms.CharField(max_length=40, label='密码', widget=forms.PasswordInput)
+class ResumeForm(forms.Form):
+    seeker_name=forms.CharField(label='姓名',max_length=20)
--- jmr/models.py
+++ jmr/models.py
@@ -10,7 +10,14 @@ class JobHunter(models.Model):
 
 class Resume(models.Model):
     JobHunter = models.ForeignKey(JobHunter, on_delete=models.CASCADE)
-    age = models.IntegerField(default=20)
+    age = models.IntegerField()
+    birthday = models.DateTimeField("birthday", default=timezone.now)
+    major = models.CharField(max_length=40, default='aa')
+    education = models.CharField(max_length=10, default='aa')
+    endowment = models.CharField(max_length=20, default='aa')
+    tel = models.IntegerField(default=2222)
+    race = models.CharField(max_length=4, default='aa')
+    family_location = models.CharField(max_length=100, default='aa')
 
 
 # Create your models here.
@@ -18,6 +25,7 @@ class Enterprise(models.Model):
     enterprise_name = models.CharField(max_length=40)
     enterprise_address = models.CharField(max_length=80)
     enterprise_info = models.CharField(max_length=200)
+    enterprise_username = models.CharField(max_length=20, default="aa")
 
     def __str__(self):
         return self.enterprise_name
